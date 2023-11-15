6f176946e09577074a05c105a069d237bd700b3b
Magpier <garson.ao@gmail.com>
2023-11-16 01:27:31 +0300

    Версия решение 1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,36 @@
 package app.model;
 
+import app.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     @Autowired
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+
+    @Autowired
+    @Qualifier("Dog")
     private Animal animal;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
 }
